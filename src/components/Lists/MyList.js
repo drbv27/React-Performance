36f4ca41ecb3e758e0e41fb042ae6b6efdf28691
Diego <drbv27@gmail.com>
2022-01-11 08:32:56 -0500

    memo with children props

--- src/components/Lists/MyList.js
+++ src/components/Lists/MyList.js
@@ -1,11 +1,18 @@
+import { isEqual } from "lodash";
 import { memo } from "react";
-const Li = memo(({ fullname }) => {
+
+/* const Li = memo(({ fullname }) => {
   console.log(`renderizando ${fullname}`);
   return <li>{fullname}</li>;
-});
+}); */
+
+const Li = memo(({ children }) => {
+  console.log(`renderizando ${children}`);
+  return <li>{children}</li>;
+}, isEqual);
 
 /* const MLi = memo(Li); */ //una forma es aplicando memo al componente
-const MyList = ({ data }) => {
+/* const MyList = ({ data }) => {
   console.log("renderizando lista");
   return (
     <ul>
@@ -14,6 +21,18 @@ const MyList = ({ data }) => {
       ))}
     </ul>
   );
+}; */
+const MyList = ({ data }) => {
+  console.log("renderizando lista");
+  return (
+    <ul>
+      {data.map((x) => (
+        <Li key={x.name + x.lastname}>
+          {x.name} {x.lastname}
+        </Li>
+      ))}
+    </ul>
+  );
 };
 
-export default MyList;
+export default memo(MyList);
