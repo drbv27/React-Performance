a616508d39a4b40322bac5bae795437faa1f8647
Diego <drbv27@gmail.com>
2022-01-11 07:59:00 -0500

    memo

--- src/components/Lists/MyList.js
+++ src/components/Lists/MyList.js
@@ -1,24 +1,19 @@
-const Li = ({ fullname }) => {
-  console.log(`renderizando ${fullname}`)
-  return (
-    <li>
-      {fullname}
-    </li>
-  )
-}
+import { memo } from "react";
+const Li = memo(({ fullname }) => {
+  console.log(`renderizando ${fullname}`);
+  return <li>{fullname}</li>;
+});
 
+/* const MLi = memo(Li); */ //una forma es aplicando memo al componente
 const MyList = ({ data }) => {
-  console.log('renderizando lista')
+  console.log("renderizando lista");
   return (
     <ul>
-      {data.map(x =>
-        <Li
-          key={x.name + x.lastname}
-          fullname={`${x.name} ${x.lastname}`}
-        />
-      )}
+      {data.map((x) => (
+        <Li key={x.name + x.lastname} fullname={`${x.name} ${x.lastname}`} />
+      ))}
     </ul>
-  )
-}
+  );
+};
 
-export default MyList
+export default MyList;
